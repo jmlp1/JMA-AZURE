7ef128187a12a4a3cc1b7068a0c944aab1380abc
jmlp1 <67766356+jmlp1@users.noreply.github.com>
2024-08-03 05:27:39 +0100

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,6 +1,6 @@
 data "azurerm_key_vault" "akv" {
   name                = var.azurerm_keyvault
-  resource_group_name = var.azure_resource_group
+  resource_group_name = var.azurerm_resource_group
 }
 
 data "azurerm_key_vault_secret" "ssh_private_key" {
@@ -11,4 +11,12 @@ data "azurerm_key_vault_secret" "ssh_private_key" {
 data "azurerm_key_vault_secret" "ssh_public_key" {
   name         = "ssh-public-key"
   key_vault_id = data.azurerm_key_vault.akv.id
+}
+
+data "azurerm_resource_group" "arg" {
+  name = var.azurerm_resource_group
+}
+
+output "id" {
+  value = data.azurerm_resource_group.arg.id
 }
